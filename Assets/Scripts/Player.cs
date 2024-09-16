8746477e1c72f95ecdacdb954abd5f84ff71e452
Hank Xu <53340720+hankxu1212@users.noreply.github.com>
2024-09-16 19:03:10 -0400

    ray to cursor2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
     [SerializeField] private float sprintSpeed = 20;
     [SerializeField] private GameInputManager gameInputManager;
     [SerializeField] private Animator animator;
-
+    
+    private Vector3 cursorDirection;
+    
     private void Awake()
     {
     }
@@ -15,8 +17,12 @@ public class Player : MonoBehaviour
     private void Update()
     {
         HandleMovement();
+        RaycastToCursor();
     }
 
+    /// <summary>
+    /// Handle WASD player movement on the xz plane
+    /// </summary>
     private void HandleMovement()
     {
         Vector2 moveVec2 = gameInputManager.GetMovementVectorNormalized();
@@ -35,5 +41,29 @@ public class Player : MonoBehaviour
             animator.SetBool("Sprint", false);
             transform.Translate(moveSpeed * Time.deltaTime * moveVec3);
         }
+    }
+    
+    /// <summary>
+    /// Raycasts from player position to mouse position and outputs data to hit.
+    /// </summary>
+    private void RaycastToCursor()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.down, transform.position.y);
+
+        // intersect camera ray with a plane beneath player's feet
+        if (groundPlane.Raycast(mouseRay, out float distance))
+        {
+            Vector3 cursorWorldPosition = mouseRay.GetPoint(distance);
+            cursorDirection = (cursorWorldPosition - transform.position).normalized;
+            
+            // update forward vector with direction:
+            transform.forward = cursorDirection;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawRay(transform.position, transform.position + cursorDirection * 10);
     }
 }
